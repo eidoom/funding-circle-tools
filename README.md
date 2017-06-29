0a4f3f17e03ad03ca5fd34b7cc46afb2bfed4ebb
ryan <rim2@st-andrews.ac.uk>
2017-06-29 12:41:59 +0100

    Made program more user friendly

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Funding Circle Tools
 
 Scripts to scrape information from Funding Circle and plot the data. 
-Also some scripts to automate login and navigate to desired page.
+(Also some scripts to automate login and navigate to desired page.)
 
-Using chromedriver and Python with Selenium and Matplotlib.
+Using chromedriver and Python 3 with Selenium and Matplotlib.
 
+### Installation instructions ###
+
+* Install/check you have Python 3 installed along with packages Selenium and Matplotlib.
+
+* Install chromedriver (https://sites.google.com/a/chromium.org/chromedriver/downloads) and take a note of installation directory.
+
+* Run generate_storage_files.py and follow instructions.
+
+### Usage instructions ###
+
+* Run tracking.py to scrape information from Funding Circle website.
+
+* Run graphing.py to generate an interactive plot of scraped data (also saves a picture to graph.pdf).
--- generate_storage_files.py
+++ generate_storage_files.py
@@ -9,27 +9,45 @@ from configuration import SETTINGS_FILE, DATA_FILE
 
 
 def main():
+    print("Generating files for storing settings and scraped data...")
+
     if isfile(SETTINGS_FILE) or isfile(DATA_FILE):
-        if input("Warning: will overwrite existing files - proceed? (yes) ") != "yes":
+        if input("Warning: will overwrite existing files - proceed?") not in ("y", "Y", "yes", "Yes"):
             exit()
 
     config = ConfigParser()
 
-    config["login"] = {"username": "",
-                       "password": "",
-                       "school": "",
-                       "friend": "",
-                       "where": ""}
+    print("Please input login details to populate settings file:")
+    username = input("  Username: ")
+    password = input("  Password: ")
+    
+    print("Now please input the answers to the security questions:")
+    school = input("  What school did you attend when you were 10 years old?")
+    friend = input("  What was the name of your best friend at school?")
+    where = input("  Where did you grow up?")
 
-    config["program"] = {"chrome_driver_directory": ""}
+    chrome_driver_directory = input("Please input the full path to the chromedriver installation directory: ")
 
+    print("Generating settings file...")
+
+    config["login"] = {"username": username,
+                       "password": password,
+                       "school": school,
+                       "friend": friend,
+                       "where": where}
+
+    config["program"] = {"chrome_driver_directory": chrome_driver_directory}
+
+    print("Writing settings file...")
     with open(SETTINGS_FILE, "w+") as config_file:
         config.write(config_file)
 
+    print("Writing file to store scraped data...")
     with open(DATA_FILE, "w+") as data_file:
         data_file.write('"when","available funds","gross yield","annualised return",'
                         '"estimated return","earnings","fees","losses","net earnings"\n')
 
+    print("Finished generating storage file.")
 
 if __name__ == "__main__":
     main()
